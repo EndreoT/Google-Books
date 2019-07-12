cba6ec414383eb25ec4917db6df481c79d44924e
EndreoT <endreotm@gmail.com>
2019-07-11 17:57:09 -0700

    get nav links working

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 
+import { Link } from 'react-router-dom'
+
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
+
+      <Link className="navbar-brand" to={"/"}>
+        <strong>
+          React Google Books
+        </strong>
+      </Link>
+      <Link className="navbar-brand" to={"/"}>
+        <strong>
+          Book search
+        </strong>
+      </Link>
+      <Link className="navbar-brand" to={"/saved"}>
+        <strong>
+          Saved Books
+        </strong>
+      </Link>
     </nav>
   );
 }
