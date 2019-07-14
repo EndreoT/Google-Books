10abb8c51db43b436659aa1ed0866318730dd5f0
EndreoT <endreotm@gmail.com>
2019-07-14 14:37:38 -0700

    move ToastNotification to separate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,23 @@
 // React
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// Toastify
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-// Socket.io-client
-import openSocket from 'socket.io-client';
+
 // Components
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-
-
-const socket = openSocket({transports: ['polling']});
+import ToastNotification from './components/TostNotification'
 
 
 class App extends React.Component {
-
-  componentDidMount() {
-    this.initSocket();
-  }
-
-  initSocket = () => {
-    socket.on('book_saved', (msg) => {
-      this.notify(msg.title);
-    });
-  }
-
-  notify = (msg) => {
-    toast(
-      (<div>
-        <p>'{msg}' just saved!</p>
-      </div>)
-    );
-  };
 
   render() {
     return (
       <Router>
         <div>
           <Nav />
-          <ToastContainer />
+          <ToastNotification />
           <Switch>
             <Route exact path='/' component={Search}></Route>
             <Route exact path='/search' component={Search}></Route>
---
+++ client/src/components/TostNotification/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+// Toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// Socket.io-client
+import openSocket from 'socket.io-client';
+
+const socket = openSocket({transports: ['polling']});
+
+
+class ToastNotification extends React.Component {
+
+  componentDidMount() {
+    this.initSocket();
+  }
+
+  initSocket = () => {
+    socket.on('book_saved', (msg) => {
+      this.notify(msg.title);
+    });
+  }
+
+  notify = (msg) => {
+    toast(
+      (<div>
+        <p>'{msg}' just saved!</p>
+      </div>)
+    );
+  }
+
+  render() {
+    return <ToastContainer></ToastContainer>
+  }
+}
+
+export default ToastNotification;
